f7958e791698c710d9ee7b75008c721a31e14dd9
Murali07 <muraliselvaraj97@gmail.com>
2022-10-13 02:14:37 +0530

    day1

---
+++ config/dbConfig.js
@@ -0,0 +1,7 @@
+const mongodb = require("mongodb");
+const dbName = "food-app";
+const dbUrl = `mongodb+srv://Murali:cfMZTb8NHKC7HB2A@cluster0.8bn9yfi.mongodb.net/${dbName}`;
+
+module.exports = {mongodb, dbName, dbUrl};
+
+
---
+++ routes/index.js
@@ -0,0 +1,80 @@
+var express = require("express");
+var router = express.Router();
+
+const { mongodb, dbName, dbUrl } = require("../config/dbConfig");
+const {
+  mongoose,
+  usersModel,
+  foodModel,
+  orderModel,
+} = require("../config/dbSchema");
+const {
+  hashPassword,
+  hashCompare,
+  createToken,
+  decodeToken,
+  validateToken,
+  adminGaurd,
+} = require("../config/auth");
+
+mongoose.connect(dbUrl);
+
+
+router.get('/all-food',validateToken, async(req, res) => {
+  try{
+    let food = await foodModel.find()
+    res.send({
+      statusCode: 200,
+      food
+    })
+
+  }
+  catch(error){
+    res.send({
+      starusCode: 500,
+      message: "Internal Server Error",
+      error
+    })
+  }
+})
+
+router.post("/add-food", validateToken, adminGaurd, async(req, res) => {
+  try{
+    let food = await foodModel.create(req.body)
+    res.send({
+      statusCode: 200,
+      message: "Food Added Successfully",
+      food
+    })
+
+  }
+  catch(error){
+    res.send({
+      starusCode: 500,
+      message: "Internal Server Error",
+      error
+    })
+  }
+  
+})
+
+router.delete("/delete-food/:id", validateToken, adminGaurd, async(req, res) => {
+  try{
+    let food = await foodModel.deleteOne({_id:mongodb.ObjectId(req.params.id)})
+    res.send({
+      statusCode: 200,
+      message: "Food Deleted Successfully",      
+    })
+
+  }
+  catch(error){
+    res.send({
+      starusCode: 500,
+      message: "Internal Server Error",
+      error
+    })
+  }
+  
+})
+
+module.exports = router;
